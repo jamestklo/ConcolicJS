6b8ad3f3c5bdaea67d73220ac0a38b2bb8eea4ae
jamestklo <tklo@cs.ubc.ca>
2013-09-30 17:36:04 -0700

    initial version of code to generate XML form CVC output
    to be added: ordering of children

--- src/main/java/ca/ubc/salt/concolicjs/CVCnode.java
+++ src/main/java/ca/ubc/salt/concolicjs/CVCnode.java
@@ -1,23 +1,86 @@
 package ca.ubc.salt.concolicjs;
 
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map;
 import java.util.Set;
 
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
 public class CVCnode {
+	Map<String, CVCnode> nameToNode;
 	Set<String> aliases;
+	
 	CVCnode parent;
-	Set<CVCnode> children;
 	int order;
+	Set<CVCnode> children;
+		
+	public CVCnode(Map<String, CVCnode> nameToNode) {
+		this.nameToNode = nameToNode;
+		this.aliases = new HashSet<String>();
+		this.children = new HashSet<CVCnode>();
+		this.order = -2;
+	}
 	
 	public void addName(String name) {
 		aliases.add(name);
 	}
 	
-	public void setParent(CVCnode parent, Map<>) {
+	public CVCnode getParent() {
+		return this.parent;
+	}
+	
+	public void setParent(CVCnode parent) {
+		if (this.parent == null) {
+			this.parent = parent;
+			parent.children.add(this);
+		}
+		else if (this.parent != parent) {
+			this.parent.merge(parent);
+		}		
+	}
+	
+	public void setOrder(int order) {
+		this.order = order;
+	}
+	
+	public void merge(CVCnode node) {	
+		if (this.equals(node)) {
+			return;
+		}
+		Set<String> aliases = node.aliases;
+		Iterator<String> itr_str = aliases.iterator();
+		while (itr_str.hasNext()) {
+			nameToNode.put(itr_str.next(), this);
+		}
+		this.aliases.addAll(aliases);
+		
+		Set<CVCnode> children = node.children;
+		Iterator<CVCnode> itr_cvc = children.iterator();
+		while (itr_cvc.hasNext()) {
+			itr_cvc.next().parent = this;
+		}
+		this.children.addAll(children);
+		
+		this.setParent(node.parent);
 	}
 
+	public Element toDOM(Document document) {				
+		String[] ary = this.aliases.toArray(new String[this.aliases.size()]);
+		Element elem = document.createElement(ary[0]);			
+		Set<CVCnode> children = this.children;
+		Iterator<CVCnode> itr_cvc = children.iterator();
+		while (itr_cvc.hasNext()) {
+			elem.appendChild(itr_cvc.next().toDOM(document));				
+		}
+		return elem;
+	}	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
 	}
-
 }
--- src/main/java/ca/ubc/salt/concolicjs/XMLgenerator.java
+++ src/main/java/ca/ubc/salt/concolicjs/XMLgenerator.java
@@ -3,6 +3,7 @@ package ca.ubc.salt.concolicjs;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintStream;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
@@ -10,60 +11,52 @@ import java.util.Map;
 import java.util.Set;
 import java.util.regex.Pattern;
 
-public class XMLgenerator {
-	Map<String, CVCnode> nameToNode = new HashMap<String, CVCnode>();
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
+import org.w3c.dom.Document;
+
+public class XMLgenerator {
+	
+	Map<String, CVCnode> nameToNode = new HashMap<String, CVCnode>();
+	public XMLgenerator(String filepath) {
+		parseLogs(filepath);
+	}
+	
 	CVCnode getAlias(String name) {
 		CVCnode ret = nameToNode.get(name);
 		if (ret == null) {
-		  ret = new CVCnode();
+		  ret = new CVCnode(nameToNode);
 		  ret.addName(name);
+		  nameToNode.put(name, ret);
 		}
 		return ret; 
 	}
 	
-	void setParent(String child, String parent) {
-		setParent(getAlias(child), getAlias(parent));
-	}
-	
-	void setParent(CVCnode child, CVCnode parent) {
-		Set<String> ret = childToParent.put(child, parent);
-		if (ret != null) {	
-			parent.addAll(ret);
-			Iterator<String> itr_str = ret.iterator();
-			while (itr_str.hasNext()) {
-				String name = itr_str.next();
-				nameToAlias.put(name, parent);				
-			}
-			Set<String> grandparent = childToParent.get(ret);
-			if (grandparent != null) {
-				setParent(parent, grandparent);
-			}
-			
-			Set<Set<String>> children = parentToChildren.get(ret);
-			if (children != null) {
-				Iterator<Set<String>> itr_set = children.iterator();
-				while (itr_set.hasNext()) {
-					child = itr_set.next();
-					childToParent.put(child, parent);
-				}
-			}
-			Set<Set<String>> c1 = parentToChildren.get(parent);
-			if (c1 == null) {
-				parentToChildren.put(parent,  children);
-			}
-			else {
-				c1.addAll(children);
-			}
-			parentToChildren.remove(ret);
+	CVCnode setParent(String child, String parent) {
+		CVCnode childNode = getAlias(child);
+		CVCnode parentNode = childNode.parent;
+		if (nameToNode.get(parent) == null && parentNode != null) {
+			parentNode.addName(parent);
+			return childNode;
 		}
+		return setParent(child, getAlias(parent));
 	}
-		
 	
-	public void parseLogs(String[] args) {		
+	CVCnode setParent(String child, CVCnode parent) {
+		CVCnode childNode = getAlias(child);
+		childNode.setParent(parent);
+		return childNode;
+	}
+	
+	void parseLogs(String filepath) {		
 		try {
 						
-			BufferedReader br = new BufferedReader(new FileReader("Z:/cvc4/cvc3-output20130913.txt"));
+			BufferedReader br = new BufferedReader(new FileReader(filepath));
 			String line;
 
 			String regex0 = "(parent|children|firstChild|lastChild|following_sibling|preceding_sibling).*";
@@ -74,23 +67,42 @@ public class XMLgenerator {
 
 			int i = 0;
 			while ((line = br.readLine()) != null) {
-				if (line.length() > 6 && line.substring(0, 6).equals("ASSERT")) {
+				if (line.length() > 6 && line.substring(0, 6).equals("ASSERT")) {					
 					String relation = line.substring(7);
 					if (relation.matches(regex0)) {
 						String[] ary = p0.split(relation);
 						System.out.println(i++ +" "+ relation +" "+ ary.length);
 						if (ary.length > 4) {
-							System.out.println(ary[0] +" "+ ary[1] + " "+ ary[3]);													
+							if (ary.length > 5) {
+								System.out.println(ary[0] +" "+ ary[1] + " "+ ary[3] +" "+ ary[5]);
+							}
+							else {
+								System.out.println(ary[0] +" "+ ary[1] + " "+ ary[3]);
+							}
+							CVCnode childNode;
 							switch(ary[0]) {
 							case("parent"):
 								setParent(ary[3], ary[1]);
 								break;
 							case("children"):
+								childNode = setParent(ary[1], ary[3]);
+								childNode.order = new Integer(ary[5]);
+								break;
 							case("firstChild"):
+								childNode = setParent(ary[1], ary[3]);
+								childNode.order = 0;						
+								break;
 							case("lastChild"):
-								setParent(ary[1], ary[3]);
+								childNode = setParent(ary[1], ary[3]);
+								childNode.order = -1;						
+								break;
+							case ("preceding_sibling"):
+							case ("following_sibling"):
+								CVCnode p = new CVCnode(nameToNode);
+								setParent(ary[1], p);
+								setParent(ary[3], p);								
 								break;								
-							}							
+							}													
 						}
 					}
 					else if (relation.matches(regex1)) {
@@ -105,6 +117,48 @@ public class XMLgenerator {
 			e.printStackTrace();
 		}
 	}
-	public static void main(String[] args) {		
+	
+	public Document toDOM(Document document) {							
+		try {
+			if (document == null) {
+				document = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
+			}
+			// find root
+			Set<CVCnode> roots = new HashSet<CVCnode>();		
+			Iterator<CVCnode> itr_cvc = nameToNode.values().iterator();
+			while (itr_cvc.hasNext()) {
+				CVCnode node = itr_cvc.next();
+				if (node.getParent() == null) {
+					roots.add(node);
+				}
+			}
+			System.out.println(roots.size());
+			itr_cvc = roots.iterator();		
+			while (itr_cvc.hasNext()) {			
+				document.appendChild(itr_cvc.next().toDOM(document));
+			}						
+		}
+		catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+			
+		return document;		
+	}
+	
+	static void outXML(Document document, PrintStream out) {
+		try {
+			//XML specific representation output (exact details not relevant)
+			TransformerFactory tf = TransformerFactory.newInstance();
+			Transformer transformer = tf.newTransformer();
+			transformer.transform(new DOMSource(document), new StreamResult(out));
+		}
+		catch(Exception e) {
+			throw new RuntimeException(e);
+		}
+	}	
+	
+	public static void main(String[] args) {
+		XMLgenerator xmlg = new XMLgenerator("Z:/cvc4/cvc3-output20130913.txt");
+		outXML(xmlg.toDOM(null), System.out);
 	}
 }
