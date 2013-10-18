dc947cea647e1495935d7793ff939edef5ca0da6
jamestklo <tklo@cs.ubc.ca>
2013-10-18 15:48:22 -0700

    making process in enmulating Windows process

--- src/main/java/ca/ubc/salt/cvc/CVCsolver.java
+++ src/main/java/ca/ubc/salt/cvc/CVCsolver.java
@@ -13,60 +13,96 @@ import java.util.List;
 
 // users Java's process builder to abstract using CVC's .exe command line interface
 public class CVCsolver {
-
-	public static void main(String[] args) {
-		List<String> commandLine = new ArrayList<String>();
-		commandLine.add("Z:/cvc4/cvc3-2.4.1-win32-optimized/bin/cvc3.exe");
-		commandLine.add("+interactive");
-		//commandLine.add("Z:/cvc4/cvc3-example1.cvc");
-		ProcessBuilder builder = new ProcessBuilder(commandLine);
-		builder.redirectErrorStream(true);			
-		builder.redirectInput(ProcessBuilder.Redirect.INHERIT);
-		builder.redirectOutput(ProcessBuilder.Redirect.INHERIT);
-		//File log = new File("Z:/cvc4/log.txt");
-		//builder.redirectOutput(ProcessBuilder.Redirect.appendTo(log));
+	protected static int counter = 0;	
+	protected Process process;
+	Thread outputThread;
+	Thread inputThread;
+	Thread errorThread;	
+	protected boolean isRunning = true;
+	protected boolean isReady = false;
+	public CVCsolver(String command) {
 		try {
-			//Process process = builder.start();
-			Process process = Runtime.getRuntime().exec( "Z:/cvc4/cvc3-2.4.1-win32-optimized/bin/cvc3.exe +interactive" );
-			OutputStream inputToProcess = process.getOutputStream();
-			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(inputToProcess));			
-			writer.write("a, b:INT;\r");
-			writer.write("ASSERT a > b AND b > 0;\r");
-			writer.write("CHECKSAT;\r");			
-			//writer.close();
+			//process = Runtime.getRuntime().exec( "Z:/cvc4/cvc3-2.4.1-win32-optimized/bin/cvc3.exe +interactive" );
+			process = Runtime.getRuntime().exec(command);
+			outputThread = new OutputStreamCleaner(this, process.getOutputStream());			
+			inputThread  = new InputStreamCleaner(this, process.getInputStream());
+			errorThread  = new InputStreamCleaner(this, process.getErrorStream());
 			
-		    InputStream outputOfProcess = process.getInputStream();
-		    InputStream error = process.getErrorStream();
-		    new StreamCleaner(outputOfProcess).start();
-		    new StreamCleaner(error).start();		    
-						
-		    inputToProcess = process.getOutputStream();
-		    writer = new BufferedWriter(new OutputStreamWriter(inputToProcess));
-		    writer.write("WHERE;\r");
-		    //writer.close();
-		    
-			process.waitFor();
-		} 
+			outputThread.start();
+			inputThread.start();
+			errorThread.start();
+			//process.waitFor();
+		}
 		catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
+	public String process(String input) {
+		BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(process.getOutputStream()));
+		try {
+			writer.write(input);
+		} 
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+		String output = "";
+		return output;
+	}
+	public void quit() {
+	}
 	
-	private static class StreamCleaner extends Thread {
+	public static void main(String[] args) {
+		CVCsolver cvcs = new CVCsolver("Z:/cvc4/cvc3-2.4.1-win32-optimized/bin/cvc3.exe");
+	}
+		
+	private static class InputStreamCleaner extends Thread {
+		private CVCsolver process;
 		private InputStream input;
-		public StreamCleaner(InputStream input) {
+		public InputStreamCleaner(CVCsolver process, InputStream input) {
+			this.process = process;
 			this.input = input;
 		}
 		public void run() {
-			byte[] buffer = new byte[512];
-			try {
-				while (this.input.read(buffer) != -1) {
-					System.out.println(new String(buffer));
+			BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+			while (process.isRunning) {
+				try {										
+					String line = reader.readLine();
+					System.out.println(counter +" "+ input.available() +" "+ line.length() +" "+ line);
+					process.isReady = true;
 				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}				
 			}
-			catch (IOException e) {
-				e.printStackTrace();
+		}
+	}
+	private static class OutputStreamCleaner extends Thread {
+		protected CVCsolver process;
+		protected OutputStream output;
+		public OutputStreamCleaner(CVCsolver process, OutputStream output) {
+			this.process = process;
+			this.output = output;
+		}
+		public void run() {			
+			BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(output));
+			while (process.isRunning) {
+				try {
+					if (process.isReady) {
+						String variable = "a"+counter;
+						writer.write(variable +":INT;\n");
+						writer.write("ASSERT "+ variable +" = "+ counter +";\n");
+						//writer.write("QUERY "+ variable +" > 500;\r");
+						writer.write("WHERE;\r");
+						++counter;
+					}
+					else {
+						writer.write("a:INT;\r");
+					}
+					output.flush();
+				}
+				catch (IOException e) {
+					e.printStackTrace();
+				}
 			}
 		}
 	}
